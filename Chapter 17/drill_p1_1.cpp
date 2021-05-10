1e26e21026044e672cf530f6097d1af1f9d14410
cupofocha <ochatastesgood@gmail.com>
2021-05-10 18:49:53 +0200

    Revert "nope"
    This reverts commit ca36a87bb3692d2b4cf89fce6fb6d38881f67da6.

--- Chapter 17/drill_p1_1.cpp
+++
@@ -1,8 +0,0 @@
-#include "std_lib_facilities.h"
-
-int main()
-{
-    int* arr = new int[20];
-
-    return 0;
-}
--- Chapter 17/drill_p2_1.cpp
+++
@@ -1,8 +0,0 @@
-#include "std_lib_facilities.h"
-
-int main()
-{
-    int* p1 = new int(7);
-
-    return 0;
-}
